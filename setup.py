54561d9c2e89b8151ca068c182fadb9300367390
tmralmeida <tmr.almeida96@gmail.com>
2024-04-05 12:29:27 +0200

    package setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='class_cond_trajpred',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Class-conditioned Trajectory Prediction.',
+    author='Tiago Rodrigues de Almeida',
+    license='MIT',
+)
